b2da1abe8b0f9cc294520289968de0087e121831
Daniel Meza <daniel.meza29@yahoo.com>
2020-05-27 11:19:45 -0500

    Added delete options to EmployeeCard

--- src/components/employee/EmployeeCard.js
+++ src/components/employee/EmployeeCard.js
@@ -4,6 +4,8 @@ export default function EmployeeCard(props) {
   return (
     <div>
       Employee of the Month: {props.employee.name}!
+      <br />
+      <button type="button" onClick={() => props.deleteEmployee(props.employee.id)}>Fired!</button>
     </div>
   )
 }
