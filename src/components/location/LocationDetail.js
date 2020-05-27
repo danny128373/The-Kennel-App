1a80ef28e93df73af423cb91d59501704f4dac98
Daniel Meza <daniel.meza29@yahoo.com>
2020-05-27 14:48:27 -0500

    Refactored LocationDetail to allow deleting and redirect to main locations component

--- src/components/location/LocationDetail.js
+++ src/components/location/LocationDetail.js
@@ -5,22 +5,33 @@ import './LocationDetail.css'
 
 const LocationDetail = props => {
   const [location, setLocation] = useState({ address: "" });
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     //get(id) from AnimalManager and hang on to the data; put it into state
-    LocationManager.get(props.locationId)
-      .then(location => {
-        setLocation({
-          address: location.address
-        });
+    LocationManager.get(props.locationId).then(location => {
+      setLocation({
+        address: location.address
       });
+      setIsLoading(false);
+    });
   }, [props.locationId]);
+
+  const handleDelete = () => {
+    setIsLoading(true);
+    LocationManager.delete(props.locationId).then(() =>
+      props.history.push("/locations")
+    );
+  };
 
   return (
     <div className="card">
       <div className="card-content">
         <h3>Address: <span style={{ color: 'darkslategrey' }}>{location.address}</span></h3>
       </div>
+      <button type="button" disabled={isLoading} onClick={handleDelete}>
+        Discharge
+        </button>
     </div>
   );
 }
