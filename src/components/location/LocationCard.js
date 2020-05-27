060021e34cc477a9b4359ef770e904df253c0e81
Daniel Meza <daniel.meza29@yahoo.com>
2020-05-27 11:24:54 -0500

    Added delete option to LocationCard

--- src/components/location/LocationCard.js
+++ src/components/location/LocationCard.js
@@ -4,6 +4,8 @@ export default function LocationCard(props) {
   return (
     <address>
       {props.location.address}
+      <br />
+      <button type="button" onClick={() => props.deleteLocation(props.location.id)}>Dust this place!</button>
     </address>
   )
 }
