79edd3200ffbb70cb41ced6fc5de79853ba8a576
Daniel Meza <daniel.meza29@yahoo.com>
2020-05-29 14:01:28 -0500

    Added OwnerCard styling

--- src/components/owner/OwnerCard.js
+++ src/components/owner/OwnerCard.js
@@ -1,18 +1,22 @@
 import React from 'react'
+import './Owner.css'
 
 const OwnerCard = (props) => {
   return (
-    <h2>
-      {props.owner.name}
-      <br />
-      <small>{props.owner.phone}</small>
-      <br />
-      <button type="button"
-        onClick={() => props.history.push(`/owners/${props.owner.id}/edit`)}>
-        Edit
+    <div className="card">
+      <h2 className="card-content">
+        {props.owner.name}
+        <br />
+        <small>{props.owner.phone}</small>
+        <br />
+        <button type="button"
+          onClick={() => props.history.push(`/owners/${props.owner.id}/edit`)}>
+          Edit
       </button>
-      <button type="button" onClick={() => props.deleteOwner(props.owner.id)}>Ban this customer!</button>
-    </h2>
+        <button type="button" onClick={() => props.deleteOwner(props.owner.id)}>Ban this customer!</button>
+      </h2>
+    </div>
+
   )
 }
 
