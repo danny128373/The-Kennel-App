a64b706140789d264d502a987912e162acf1fe3e
Daniel Meza <daniel.meza29@yahoo.com>
2020-05-29 13:34:02 -0500

    Added styling to EmployeeCard by adding Employee.css

--- src/components/employee/EmployeeCard.js
+++ src/components/employee/EmployeeCard.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import './Employee.css'
 
 export default function EmployeeCard(props) {
   return (
-    <div>
-      <p>{props.employee.name}</p>
-      <p>{props.employee.position}</p>
-      <br />
-      <button type="button"
-        onClick={() => props.history.push(`/employees/${props.employee.id}/edit`)}>
-        Edit
+    <div className="card">
+      <div className="card-content">
+        <h3>{props.employee.name}</h3>
+        <p>{props.employee.position}</p>
+        <br />
+        <button type="button"
+          onClick={() => props.history.push(`/employees/${props.employee.id}/edit`)}>
+          Edit
       </button>
-      <button type="button" onClick={() => props.deleteEmployee(props.employee.id)}>Fired!</button>
+        <button type="button" onClick={() => props.deleteEmployee(props.employee.id)}>Fired!</button>
+      </div>
     </div>
   )
 }
