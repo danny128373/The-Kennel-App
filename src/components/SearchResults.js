da89e0ae652e9e97d1f91bc91cfd9c8d163cbb56
Daniel Meza <daniel.meza29@yahoo.com>
2020-06-04 20:42:59 -0500

    Can now search by name_like to any collection

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react'
-import EmployeeList from './employee/EmployeeList'
-import LocationList from './location/LocationList'
-import OwnerList from './owner/OwnerList'
 import AnimalCard from './animal/AnimalCard'
 import EmployeeCard from './employee/EmployeeCard'
+import LocationCard from './location/LocationCard'
+import OwnerCard from './owner/OwnerCard'
 
 export default function SearchResults(props) {
   console.log(props)
@@ -18,15 +17,21 @@ export default function SearchResults(props) {
       </div>
       <div>
         <h2>Matching Employees</h2>
-        <EmployeeList />
+        {props.search !== undefined ? props.search.employees.map(employee => {
+          return <EmployeeCard employee={employee} key={employee.id} />
+        }) : null}
       </div>
       <div>
         <h2>Matching Locations</h2>
-        <LocationList />
+        {props.search !== undefined ? props.search.locations.map(location => {
+          return <LocationCard location={location} key={location.id} />
+        }) : null}
       </div>
       <div>
         <h2>Matching Owners</h2>
-        <OwnerList />
+        {props.search !== undefined ? props.search.owners.map(owner => {
+          return <OwnerCard owner={owner} key={owner.id} />
+        }) : null}
       </div>
     </>
   )
